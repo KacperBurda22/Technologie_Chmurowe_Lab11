e9d4e5795d9f1fcf2408c9b779f536b7660a46c8
KacperBurda22 <129667187+KacperBurda22@users.noreply.github.com>
2023-06-01 20:02:48 +0200

    Create index.php

---
+++ html/index.php
@@ -0,0 +1,12 @@
+ <title>Moja aplikacja PHP</title>
+</head>
+<body>
+    <h1>Witaj w mojej aplikacji PHP!</h1>
+    <p>Strona startowa aplikacji.</p>
+    <?php
+    
+        $name = "Kacper";
+        echo "Witaj, $name!";
+    ?>
+</body>
+</html>
